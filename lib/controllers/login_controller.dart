8b0bebc7925307aef8cbec600b59c57398d62a5e
fajri <ryuucrowne2025@gmail.com>
2026-05-26 10:59:01 +0700

    Update login_controller.dart

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,29 +1,17 @@
 import 'package:get/get.dart';
 
-// 1. Buat abstract class atau interface
-abstract class AuthService {
-  bool validateUser(String username, String password);
-}
-
-// 2. Implementasi asli (untuk production)
-class RealAuthService implements AuthService {
-  @override
-  bool validateUser(String username, String password) {
-    return username == 'admin' && password == 'admin123';
-  }
-}
-
-// 3. Controller dengan dependensi injection
 class LoginController extends GetxController {
   var username = ''.obs;
   var password = ''.obs;
-  final AuthService authService;
-  
-  // Constructor injection
-  LoginController({required this.authService});
-  
+
+  //  LOGIC (bisa di-test)
+  bool checkLogin(String username, String password) {
+    return username == 'admin' && password == 'admin123';
+  }
+
+  //  UI (tidak di-test)
   void login() {
-    if (authService.validateUser(username.value, password.value)) {
+    if (checkLogin(username.value, password.value)) {
       Get.offAllNamed('/home');
     } else {
       Get.snackbar('Error', 'Login gagal');
