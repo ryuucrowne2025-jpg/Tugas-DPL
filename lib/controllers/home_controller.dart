b4665e17c2def1ef9043850b35ca4e3e01a1ef78
fajri <ryuucrowne2025@gmail.com>
2026-05-26 11:13:58 +0700

    Update home_controller.dart

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,93 +1,89 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
-import '../models/alumni_model.dart';
+import 'package:tugas_dpl/controllers/home_controller.dart';
+import 'package:tugas_dpl/models/alumni_model.dart';
 
-class HomeController extends GetxController {
-  var list = <Alumni>[].obs;
+class StubAlumniRepository implements AlumniRepository {
+  List<Alumni> _dataAlumni = [];
 
-  @override
-  void onInit() {
-    super.onInit();
-
-    list.addAll([
+  StubAlumniRepository() {
+    _dataAlumni = [
       Alumni(
-        nama: "Catur Rahmani Oktavia",
+        name: "Catur Rahmani Oktavia",
         email: "catur@gmail.com",
-        noHp: "081234567890",
+        nobp: "081234567890",
         linkedin: "linkedin.com/catur",
         instagram: "@catur",
         facebook: "catur.fb",
         tiktok: "@caturtok",
-        tempatKerja: "PT Maju Jaya",
-        alamatKerja: "Jakarta",
-        posisi: "Staff Akuntansi",
-        jenis: "Swasta",
-        sosmedPerusahaan: "@ptmajujaya",
       ),
-      Alumni(
-        nama: "Indayati",
-        email: "indayati@gmail.com",
-        noHp: "081234567891",
-        linkedin: "linkedin.com/indayati",
-        instagram: "@inda",
-        facebook: "inda.fb",
-        tiktok: "@indatok",
-        tempatKerja: "Instansi Pemerintah",
-        alamatKerja: "Bandung",
-        posisi: "Admin",
-        jenis: "PNS",
-        sosmedPerusahaan: "@pemda",
-      ),
-      Alumni(
-        nama: "Assa Idhika",
-        email: "assa@gmail.com",
-        noHp: "081234567892",
-        linkedin: "linkedin.com/assa",
-        instagram: "@assa",
-        facebook: "assa.fb",
-        tiktok: "@assatok",
-        tempatKerja: "Usaha Sendiri",
-        alamatKerja: "Bekasi",
-        posisi: "Owner",
-        jenis: "Wirausaha",
-        sosmedPerusahaan: "@usahaku",
-      ),
-      Alumni(
-        nama: "Yuli ka Yanti",
-        email: "yuli@gmail.com",
-        noHp: "081234567893",
-        linkedin: "linkedin.com/yuli",
-        instagram: "@yuli",
-        facebook: "yuli.fb",
-        tiktok: "@yulitok",
-        tempatKerja: "PT Sejahtera",
-        alamatKerja: "Surabaya",
-        posisi: "Staff Keuangan",
-        jenis: "Swasta",
-        sosmedPerusahaan: "@sejahtera",
-      ),
-      Alumni(
-        nama: "Gunawan",
-        email: "gunawan@gmail.com",
-        noHp: "081234567894",
-        linkedin: "linkedin.com/gunawan",
-        instagram: "@gunawan",
-        facebook: "gunawan.fb",
-        tiktok: "@gunawantok",
-        tempatKerja: "PT Nusantara",
-        alamatKerja: "Jakarta",
-        posisi: "Supervisor",
-        jenis: "Swasta",
-        sosmedPerusahaan: "@nusantara",
-      ),
-    ]);
+    ];
   }
 
-  void tambah(Alumni data) => list.add(data);
-
-  void hapus(int index) => list.removeAt(index);
-
-  void edit(int index, Alumni dataBaru) {
-    list[index] = dataBaru;
-    list.refresh();
+  @override
+  Future<List<Alumni>> fetchAllAlumni() async {
+    return _dataAlumni;
   }
+
+  @override
+  Future<void> addAlumni(Alumni alumni) async {
+    _dataAlumni.add(alumni);
+  }
+
+  @override
+  Future<void> deleteAlumni(String nobp) async {
+    _dataAlumni.removeWhere((a) => a.nobp == nobp);
+  }
+}
+
+void main() {
+  setUp(() {
+    Get.reset();
+  });
+
+  test('Test 1: Load alumni berhasil mengambil data', () async {
+    StubAlumniRepository stubRepo = StubAlumniRepository();
+    HomeController controller = HomeController(repository: stubRepo);
+
+    await controller.loadAlumni();
+
+    expect(controller.alumniList.length, 1);
+    expect(controller.alumniList[0].name, "Catur Rahmani Oktavia");
+  });
+
+  test('Test 2: Tambah alumni baru berhasil', () async {
+    StubAlumniRepository stubRepo = StubAlumniRepository();
+    HomeController controller = HomeController(repository: stubRepo);
+
+    await controller.loadAlumni();
+    expect(controller.alumniList.length, 1);
+
+    Alumni newAlumni = Alumni(
+      name: "Budi Santoso",
+      email: "budi@gmail.com",
+      nobp: "089876543210",
+      linkedin: "linkedin.com/budi",
+      instagram: "@budi",
+      facebook: "budi.fb",
+      tiktok: "@buditok",
+    );
+
+    await controller.addAlumni(newAlumni);
+    await controller.loadAlumni();
+
+    expect(controller.alumniList.length, 2);
+  });
+
+  test('Test 3: Hapus alumni berhasil berdasarkan nobp', () async {
+    StubAlumniRepository stubRepo = StubAlumniRepository();
+    HomeController controller = HomeController(repository: stubRepo);
+
+    await controller.loadAlumni();
+    expect(controller.alumniList.length, 1);
+
+    await controller.deleteAlumni("081234567890");
+    await controller.loadAlumni();
+
+    expect(controller.alumniList.length, 0);
+  });
 }
