a0ea5eba8ce89a2e73554191e6b21687f42af934
fajri <ryuucrowne2025@gmail.com>
2026-06-03 12:30:18 +0700

    Update login_controller.dart

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,30 +1,34 @@
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
-  var username = ''.obs;
-  var password = ''.obs;
-
-  // ===============================
-  // STUB FUNCTION (UNTUK TESTING)
-  // ===============================
+  RxString username = ''.obs;
+  RxString password = ''.obs;
+  
   bool checkLogin(String username, String password) {
     if (username == 'admin' && password == 'admin123') {
       return true;
     }
+
     return false;
   }
 
-  // ===============================
-  // IMPLEMENTASI LOGIN
-  // ===============================
-  void login() {
-    bool isValid = checkLogin(username.value, password.value);
+    bool isValid = checkLogin(
+      username.value,
+      password.value,
+    );
 
     if (isValid) {
-      Get.snackbar('Success', 'Login berhasil');
+      Get.snackbar(
+        'Success',
+        'Login berhasil',
+      );
+
       Get.offAllNamed('/home');
     } else {
-      Get.snackbar('Error', 'Username atau Password salah');
+      Get.snackbar(
+        'Error',
+        'Username atau Password salah',
+      );
     }
   }
 }
