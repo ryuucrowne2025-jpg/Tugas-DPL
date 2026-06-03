57b3ea75773b9f7dab47eee7a24cb74aac2797f5
fajri <ryuucrowne2025@gmail.com>
2026-06-03 11:52:02 +0700

    Update login_test.dart

--- lib/login_test.dart
+++ lib/login_test.dart
@@ -4,28 +4,11 @@ class LoginController extends GetxController {
   var username = ''.obs;
   var password = ''.obs;
 
-  // ================================
-  // LOGIC UTAMA (testable)
-  // ================================
+  // TAMBAHAN (ini yang akan hijau)
   bool checkLogin(String username, String password) {
     return username == 'admin' && password == 'admin123';
   }
 
-  // ================================
-  // VERSI LEBIH TESTABLE (bau stub)
-  // ================================
-  bool checkLoginWithCustomData(
-    String username,
-    String password, {
-    String correctUsername = 'admin',
-    String correctPassword = 'admin123',
-  }) {
-    return username == correctUsername && password == correctPassword;
-  }
-
-  // ================================
-  // UI FUNCTION (tidak di-test)
-  // ================================
   void login() {
     if (checkLogin(username.value, password.value)) {
       Get.offAllNamed('/home');
