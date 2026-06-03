ca0b2a9f9a08253d3ab2b7c633ffe34cdb7a614b
fajri <ryuucrowne2025@gmail.com>
2026-06-03 12:37:45 +0700

    Update login_test.dart

--- lib/login_test.dart
+++ lib/login_test.dart
@@ -8,15 +8,67 @@ void main() {
     controller = LoginController();
   });
 
-  test(
-    'Test login berhasil',
-    () {
-      bool result = controller.checkLogin(
-        'admin',
-        'admin123',
-      );
+  group('LoginController Unit Test', () {
 
-      expect(result, true);
-    },
-  );
+    test(
+      'Test 1: Login berhasil jika username dan password benar',
+      () {
+        bool result = controller.checkLogin(
+          'admin',
+          'admin123',
+        );
+
+        expect(result, true);
+      },
+    );
+
+    test(
+      'Test 2: Login gagal jika username salah',
+      () {
+        bool result = controller.checkLogin(
+          'user',
+          'admin123',
+        );
+
+        expect(result, false);
+      },
+    );
+
+    test(
+      'Test 3: Login gagal jika password salah',
+      () {
+        bool result = controller.checkLogin(
+          'admin',
+          'passwordSalah',
+        );
+
+        expect(result, false);
+      },
+    );
+
+    test(
+      'Test 4: Login gagal jika username dan password kosong',
+      () {
+        bool result = controller.checkLogin(
+          '',
+          '',
+        );
+
+        expect(result, false);
+      },
+    );
+
+    test(
+      'Test 5: Login gagal jika hanya username diisi',
+      () {
+        bool result = controller.checkLogin(
+          'admin',
+          '',
+        );
+
+        expect(result, false);
+      },
+    );
+
+  });
 }
