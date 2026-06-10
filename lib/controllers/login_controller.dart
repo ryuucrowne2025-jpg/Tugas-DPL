f3632efc433b8fc18733989543914a5b5084e8ce
fajri <ryuucrowne2025@gmail.com>
2026-06-10 11:57:28 +0700

    Update login_controller.dart

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,33 +1,28 @@
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
+   final AuthRepository repository;
+
+  LoginController({required this.repository});
+
   RxString username = ''.obs;
   RxString password = ''.obs;
   
   bool checkLogin(String username, String password) {
-    if (username == 'admin' && password == 'admin123') {
-      return true;
-    }
-
-    return false;
+    return repository.login(username, password);
   }
 
+  void login() {
     bool isValid = checkLogin(
       username.value,
       password.value,
     );
 
     if (isValid) {
-      Get.snackbar(
-        'Success',
-        'Login berhasil',
-      );
-
+     Get.snackbar('Success', 'Login berhasil');
       Get.offAllNamed('/home');
     } else {
-      Get.snackbar(
-        'Error',
-        'Username atau Password salah',
+     Get.snackbar('Error', 'Username atau Password salah');
       );
     }
   }
